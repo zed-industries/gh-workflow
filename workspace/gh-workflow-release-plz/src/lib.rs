28bdd991e2e64aa0c7c4db91796554cda5fbfe59
Tushar Mathur <tusharmath@gmail.com>
2024-11-11 12:03:11 +0530

    chore: lint fixes

--- workspace/gh-workflow-release-plz/src/lib.rs
+++ workspace/gh-workflow-release-plz/src/lib.rs
@@ -2,19 +2,26 @@ use gh_workflow::{Step, Use};
 
 #[derive(Clone, Default)]
 pub struct ReleasePlz {
-    /// The release-plz command to run. Accepted values: release-pr, release. (By default it runs both commands).
+    /// The release-plz command to run. Accepted values: release-pr, release.
+    /// (By default it runs both commands).
     pub command: Option<Command>,
 
-    /// Registry where the packages are stored. The registry name needs to be present in the Cargo config. If unspecified, the publish field of the package manifest is used. If the publish field is empty, crates.io is used.
+    /// Registry where the packages are stored. The registry name needs to be
+    /// present in the Cargo config. If unspecified, the publish field of the
+    /// package manifest is used. If the publish field is empty, crates.io is
+    /// used.
     pub registry: Option<String>,
 
-    /// Path to the Cargo.toml of the project you want to update. Both Cargo workspaces and single packages are supported. (Defaults to the root directory).
+    /// Path to the Cargo.toml of the project you want to update. Both Cargo
+    /// workspaces and single packages are supported. (Defaults to the root
+    /// directory).
     pub manifest_path: Option<String>,
 
     /// Release-plz version to use. E.g. 0.3.70. (Default: latest version).
     pub version: Option<String>,
 
-    /// Release-plz config file location. (Defaults to release-plz.toml or .release-plz.toml).
+    /// Release-plz config file location. (Defaults to release-plz.toml or
+    /// .release-plz.toml).
     pub config: Option<String>,
 
     /// Token used to publish to the cargo registry.
@@ -30,11 +37,11 @@ pub enum Command {
     Release,
 }
 
-impl ToString for Command {
-    fn to_string(&self) -> String {
+impl std::fmt::Display for Command {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Command::ReleasePR => "release-pr".to_string(),
-            Command::Release => "release".to_string(),
+            Command::ReleasePR => write!(f, "release-pr"),
+            Command::Release => write!(f, "release"),
         }
     }
 }
@@ -45,11 +52,11 @@ pub enum Backend {
     Gitea,
 }
 
-impl ToString for Backend {
-    fn to_string(&self) -> String {
+impl std::fmt::Display for Backend {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Backend::GitHub => "github".to_string(),
-            Backend::Gitea => "gitea".to_string(),
+            Backend::GitHub => write!(f, "github"),
+            Backend::Gitea => write!(f, "gitea"),
         }
     }
 }
